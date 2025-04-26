fb084858a86da83cf64e83c37510af510dff269c
ririsahu <ririsahu1600@gmail.com>
2025-04-26 16:31:31 +0530

    update js

--- script.js
+++ script.js
@@ -5,10 +5,12 @@ let flag = 0;
 btn.addEventListener("click" , function() {
     if(flag == 0 ){
         bulb.style.backgroundColor = "yellow" 
+        btn.innerHTML = "OFF"
         flag = 1;
     }
     else{
         bulb.style.backgroundColor = "transparent" 
+        btn.innerHTML = "ON"
         flag = 0;
     }
     
